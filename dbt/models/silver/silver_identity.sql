ffbf1b271b2cc0620901a6950d025e4523b94ceb
Linda.Z <linda.linda080295@gmail.com>
2026-07-09 23:06:46 +0200

    add comment for silver layer

--- dbt/models/silver/silver_identity.sql
+++ dbt/models/silver/silver_identity.sql
@@ -1,3 +1,9 @@
+-- Column selection rationale:
+-- 1. Dropped any column with >10% nulls (see analyses/transactions_column_missingness_check.sql)
+-- 2. Kept columns identified by the Kaggle IEEE-CIS community as most predictive
+--    (TransactionAmt, ProductCD, card1-6, addr1-2, email domains, C1-C6, M1-M9)
+-- 3. No additional low-null anonymized columns sampled from this table
+
 SELECT
     TransactionID,
     id_01,id_02,
